c4b1386c5ed0dc9d0087be54faf387cbac7f92da
ngocduy1226 <lamngocduy2001@gmail.com>
2023-02-23 14:24:18 +0700

    Cai dat cac handler truy xuat CSDL

--- app.js
+++ app.js
@@ -1,32 +1,33 @@
-const express = require("express");
-const cors = require("cors");
-const contactsRouter = require("./app/routes/contact.route")
-const ApiError = require("./app/api-error");
+const express = require('express');
+const cors = require('cors');
+const { request, application } = require('express');
+
+const contactsRouter = require('./app/routes/contact.route');
+const ApiError = require('./app/api-error');
 const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.json({ message: "Welcome to contact book application." });
+});
 
 app.use("/api/contacts", contactsRouter);
 
 // handle 404 response
 app.use((req, res, next) => {
-     //chay khi khong co route dc dinh nghia nao khop voi yeu cau. goi next() de chuyen san middleware xu ly loi
-     return next(new ApiError(404, "Resource not found"));
+    // Code ở đây sẽ chạy khi không có route được định nghĩa nào khớp với yêu cầu
+    // Gọi next() để chuyển sang middleware xử lý lỗi
+    return next(new ApiError(404, "Resource not found"));
 });
-//define error-handling middleware last, after other app.use() and routes calls
+// define error-handling middleware last, after other app.use() and routes calls
 app.use((err, req, res, next) => {
-    //Middleware xu ly loi tap trung
-    //trong cac doan code xu ly o cac route, goi next(error) se chuyen ve middeware xu ly loi nay
+    // Middleware xử lý lỗi tập trung.
+    // Trong các đoạn code xử lý ở các route, gọi next(error) sẽ chuyển về middleware xử lý lỗi này
     return res.status(err.statusCode || 500).json({
-        message:err.message || "Internal Server Error",
-    });
-});
-app.use(cors());
-app.use(express.json());
-app.use("/api/contacts", contactsRouter);
-
-app.get("/", (req,res) => {
-    res.json({
-        message: "Welcome to contact book application."
+        message: err.message || "Internal Server Error",
     });
 });
 
-module.exports =  app;
+module.exports = app;
